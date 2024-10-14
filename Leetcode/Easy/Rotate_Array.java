aa93f271341fde321cd9294eded536ce57c0480c
Subhaharini733 <subhaharini733@gmail.com>
2024-10-14 14:48:16 +0530

    Create Rotate_Array.java

---
+++ Leetcode/Easy/Rotate_Array.java
@@ -0,0 +1,42 @@
+// Leetcode -189- Rotate_Array
+
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter array elements: ");
+        for (int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter k value: ");
+        int k = sc.nextInt();
+        func(arr, n, k);
+        System.out.println("Rotated Array: ");
+        printArray(arr);
+        sc.close();
+    }
+    static void func(int[] arr, int n, int k){
+        k = k % n;
+        reverse(arr, 0, n-1);
+        reverse(arr, 0, k-1);
+        reverse(arr, k, n-1);
+    }
+    static void reverse(int[] arr, int start, int end){
+        while (start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    static void printArray(int[] arr){
+        for (int a : arr){
+            System.out.print(a + " ");
+        }
+        System.out.println();
+    }
+}
