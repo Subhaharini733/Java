8bd1e18927908f5f9619b368ed943658b93dfcce
Subhaharini733 <subhaharini733@gmail.com>
2024-10-21 01:20:35 +0530

    Create Student.java

---
+++ Cycle programs/Concept-wise/Student ID data/Student.java
@@ -0,0 +1,23 @@
+public class Student{
+private int id;
+private String name;
+private String address;
+public void setId(int id){
+this.id=id;
+}
+public int getId(){
+return this.id;
+}
+public void setName(String name){
+this.name=name;
+}
+public String getName(){
+return this.name;
+}
+public void setAddress(String address){
+this.address=address;
+}
+public String getAddress(){
+return this.address;
+}
+}
