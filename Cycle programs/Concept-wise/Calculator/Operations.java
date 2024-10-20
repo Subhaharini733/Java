a6e105f5c6dcfa7f10483bf94cefe334137c30f9
Subhaharini733 <subhaharini733@gmail.com>
2024-10-21 01:19:32 +0530

    Create Operations.java

---
+++ Cycle programs/Concept-wise/Calculator/Operations.java
@@ -0,0 +1,26 @@
+public class Operations
+{
+	public int add(int a, int b)
+	{
+		System.out.println("In addition method.....");
+		return (a+b);
+	}
+	
+	public int subtract(int a, int b)
+	{
+		System.out.println("In Subtraction method.....");
+		return (a-b);
+	}
+	
+	public int product(int a, int b)
+	{
+		System.out.println("In Product method.....");
+		return (a*b);
+	}
+
+	public float division(int a, int b)
+	{
+		System.out.println("In Division method.....");
+		return (a/b);
+	}	
+}
